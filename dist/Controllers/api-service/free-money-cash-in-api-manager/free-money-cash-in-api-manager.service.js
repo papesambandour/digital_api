03f58020a198b752979f6d4fd4c00e6e334e4374
Moussa Ndour <moussa.ndour@intech.sn>
2023-07-11 08:00:57 +0000

    deployment Tue Jul 11 08:00:57 GMT 2023

--- dist/Controllers/api-service/free-money-cash-in-api-manager/free-money-cash-in-api-manager.service.js
+++ dist/Controllers/api-service/free-money-cash-in-api-manager/free-money-cash-in-api-manager.service.js
@@ -45,8 +45,8 @@ class FreeMoneyCashInApiManagerService extends api_manager_interface_service_1.A
             currency: 'XOF',
             agentmsisdn: process.env.FREE_MSISDN_NUMBER,
             customermsisdn: transaction.phone,
-            externaltransactionid: transaction.transactionId,
-            externalid: transaction.transactionId,
+            externaltransactionid: transaction.id.toString(),
+            externalid: transaction.id.toString(),
             username: process.env.FREE_MSISDN_NUMBER,
             password: process.env.FREE_MSISDN_PASSWORD,
         };
